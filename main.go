26f6823bc8b849f90b81edfe4ab4e36239d4955f
Upendra Dev <168770843+novicescript@users.noreply.github.com>
2024-06-06 06:42:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "https://timelapse.onrender.com",
 		AllowHeaders: "Origin,Content-Type,Accept",
 	}))
 
